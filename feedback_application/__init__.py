ba558ad8224bf16626f56bbeeea91eef9aa4a196
Noah MacDonald <noahmacdonald@Noah-MacDonalds-MacBook-Air.local>
2019-08-23 12:05:22 -0700

    added AP

--- feedback_application/__init__.py
+++ feedback_application/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
+csrf = CSRFProtect(app)
 application = Flask(__name__)
 application.config['SECRET_KEY'] = 'verysecretbytes'
+csrf.init_app(application)
 
 from feedback_application import routes
